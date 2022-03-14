a632d9b0a0bacf63ba4c29fddee670204abbc80c
rupam bumba <bybumba@gmail.com>
2022-03-14 16:14:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+
+const express = require("express");
+const app = express()
+
+const http = require("http");
+const httpServer = http.createServer(app);
+const io = require("socket.io")(httpServer, {cors: {origin: "*",}, });
+
+const { v4 } = require("uuid") 
+
+
+
+app.set("view engine","ejs")
+app.use(express.static("public"))
+
+app.get("/" , (req,res) =>{
+    res.redirect(`${v4()}`)
+})
+
+
+app.get("/:room",(req,res)=>{
+    res.render('room' ,{roomId : req.params.room})
+})
+
+
+io.on("connection",(socket) => {
+    socket.on("join-room" , (roomId,userId) =>{
+        console.log(roomId,userId);
+        socket.join(roomId)
+        socket.to(roomId).emit('user-connected', userId)
+
+        socket.on("disconnect" , ()=>{
+            socket.to(roomId).emit('user-disconnect' , userId)
+        })
+    })
+ 
+})
+
+
+
+httpServer.listen(3000, () => {
+    console.log("............");
+});
